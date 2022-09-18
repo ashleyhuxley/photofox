b2ca6ef34014567e67229e3f3de7769430ce848e
Ashley Huxley <ash@huxley.im>
2022-09-19 00:13:42 +0100

    Initial batch correction code

---
+++ PhotoFox.CmdLine/PhotoFoxConfig.cs
@@ -0,0 +1,10 @@
+﻿using PhotoFox.Storage;
+using System.Configuration;
+
+namespace PhotoFox.CmdLine
+{
+    public class PhotoFoxConfig : IStorageConfig
+    {
+        public string StorageConnectionString => ConfigurationManager.AppSettings["StorageConnectionString"];
+    }
+}
---
+++ PhotoFox.CmdLine/Program.cs
@@ -0,0 +1,53 @@
+﻿using PhotoFox.Core;
+using PhotoFox.Model;
+using PhotoFox.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoFox.CmdLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync()
+        {
+            var config = new PhotoFoxConfig();
+            var metaDataStorage = new PhotoMetadataStorage(config);
+
+            var photos = new List<PhotoMetadata>();
+
+            await foreach (var photo in metaDataStorage.GetAllPhotos())
+            {
+                photos.Add(photo);
+            }
+
+            var batch = 0;
+            var i = 0;
+
+            foreach (var photo in photos.OrderBy(p => p.DateTaken))
+            {
+                var batchPhoto = new PhotoInBatch
+                {
+                    PartitionKey = batch.ToBatchId(),
+                    RowKey = photo.RowKey,
+                    UtcDate = photo.DateTaken
+                };
+
+                Console.WriteLine($"{batchPhoto.PartitionKey} - {batchPhoto.RowKey}");
+
+                i++;
+                if (i == 10)
+                {
+                    i = 0;
+                    batch++;
+                }
+            }
+        }
+    }
+}
