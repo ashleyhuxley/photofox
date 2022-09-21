4445f06d7947dead9f48eea386b2a642c3fda435
Ashley Huxley <gitlab@huxley.im>
2022-09-21 19:32:18 +0100

    Minor refactor

--- PhotoFox.Storage/Blob/PhotoFileStorage.cs
+++ PhotoFox.Storage/Blob/PhotoFileStorage.cs
@@ -8,15 +8,39 @@ namespace PhotoFox.Storage.Blob
     {
         private readonly IStorageConfig config;
 
+        private const string ThumbnailsContainer = "thumbnails";
+
+        private const string PhotosContainer = "photos";
+
         public PhotoFileStorage(IStorageConfig config)
         {
             this.config = config;
         }
 
-        public async Task<BinaryData> GetFileAsync(string id)
+        public async Task<BinaryData> GetThumbnailAsync(string id)
+        {
+            return await GetFileAsync(id, ThumbnailsContainer);
+        }
+
+        public async Task<BinaryData> GetPhotoAsync(string id)
+        {
+            return await GetFileAsync(id, PhotosContainer);
+        }
+
+        public async Task PutThumbnailAsync(string id, BinaryData data)
+        {
+            await PutFileAsync(id, data, ThumbnailsContainer);
+        }
+
+        public async Task PutPhotoAsync(string id, BinaryData data)
+        {
+            await PutFileAsync(id, data, PhotosContainer);
+        }
+
+        private async Task<BinaryData> GetFileAsync(string id, string containerName)
         {
             var client = new BlobServiceClient(this.config.StorageConnectionString);
-            var container = client.GetBlobContainerClient("thumbnails");
+            var container = client.GetBlobContainerClient(containerName);
 
             var blob = container.GetBlobClient(id);
 
@@ -27,6 +51,12 @@ namespace PhotoFox.Storage.Blob
 
             var res = await blob.DownloadContentAsync();
             return res.Value.Content;
+        }
+
+        private async Task PutFileAsync(string id, BinaryData data, string containerName)
+        {
+            var client = new BlobServiceClient(this.config.StorageConnectionString);
+            var container = client.GetBlobContainerClient(containerName);
         }
     }
 }
