474b5efd7f42e190198eafeb1d17dd45024d08fc
Ashley Huxley <ash@huxley.im>
2022-09-24 13:45:34 +0100

    Improved hashing storage

---
+++ PhotoFox.Core/Extensions/StringExtenstions.cs
@@ -0,0 +1,10 @@
+﻿namespace PhotoFox.Core.Extensions
+{
+    public static class StringExtenstions
+    {
+        public static string ToHashPartitionKey(this string hash)
+        {
+            return hash.Substring(0, 2);
+        }
+    }
+}
