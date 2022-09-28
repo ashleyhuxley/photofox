b1bd785a97d1344cf97e8645d5e569f7a527494c
Ashley Huxley <gitlab@huxley.im>
2022-09-28 23:17:16 +0100

    Refactored Photo Album retrieval into service

---
+++ PhotoFox.Model/PhotoAlbum.cs
@@ -0,0 +1,13 @@
+﻿namespace PhotoFox.Model
+{
+    public class PhotoAlbum
+    {
+        public string AlbumId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string CoverPhotoId { get; set; }
+    }
+}
