6edf93e1c8bc27fb2c42357f41fa3d4a0e24d16c
Ashley Huxley <gitlab@huxley.im>
2022-09-30 19:46:38 +0100

    Created branch for refactoring into services

---
+++ PhotoFox.Model/Photo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PhotoFox.Model
+{
+    public class Photo
+    {
+        public string PhotoId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DateTaken { get; set; }
+        public string Iso { get; set; }
+        public string Aperture { get; set; }
+        public string FocalLength { get; set; }
+        public string Device { get; set; }
+        public int? Orientation { get; set; }
+        public string Exposure { get; set; }
+        public double? GeolocationLattitude { get; set; }
+        public double? GeolocationLongitude { get; set; }
+        public string FileHash { get; set; }
+    }
+}
--- PhotoFox.Services/PhotoService.cs
+++ PhotoFox.Services/PhotoService.cs
@@ -1,7 +1,40 @@
-﻿namespace PhotoFox.Services
+﻿using PhotoFox.Model;
+using PhotoFox.Storage.Table;
+using System;
+using System.Collections.Generic;
+
+namespace PhotoFox.Services
 {
-    public class PhotoService
+    public class PhotoService : IPhotoService
     {
-        
+        private readonly IPhotoMetadataStorage photoMetadataStorage;
+
+        public PhotoService(IPhotoMetadataStorage photoMetadataStorage)
+        {
+            this.photoMetadataStorage = photoMetadataStorage;
+        }
+
+        public async IAsyncEnumerable<Photo> GetPhotosByDateTaken(DateTime dateTaken)
+        {
+            await foreach (var photo in this.photoMetadataStorage.GetPhotosByDate(dateTaken))
+            {
+                yield return new Photo
+                {
+                    Aperture = photo.Aperture,
+                    DateTaken = photo.UtcDate,
+                    Description = photo.Description,
+                    Device = photo.Device,
+                    Exposure = photo.Exposure,
+                    FileHash = photo.FileHash,
+                    FocalLength = photo.FocalLength,
+                    GeolocationLattitude = photo.GeolocationLattitude,
+                    GeolocationLongitude = photo.GeolocationLongitude,
+                    Iso = photo.Iso,
+                    Orientation = photo.Orientation,
+                    Title = photo.Title,
+                    PhotoId = photo.RowKey
+                };
+            }
+        }
     }
 }
