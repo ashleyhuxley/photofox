6f0cb09948ab4bfc376562d37ff1908318b9b526
Ashley Huxley <git@huxley.im>
2023-03-24 12:58:12 +0000

    Refactor to allow full sized images and thumbnails to be requested from ImageStorage

--- Photofox.Android/app/src/main/java/com/fionasapphire/photofox/storage/ImageStorage.kt
+++ Photofox.Android/app/src/main/java/com/fionasapphire/photofox/storage/ImageStorage.kt
@@ -7,10 +7,18 @@ class ImageStorage
     constructor(private val connectionString: String)
 {
     fun getThumbnail(imageId: String): ByteArray {
+        return getResource(imageId, "thumbnails")
+    }
+
+    fun getImage(imageId: String): ByteArray {
+        return getResource(imageId, "images")
+    }
+
+    private fun getResource(imageId: String, containerName: String) : ByteArray {
         val account = CloudStorageAccount.parse(connectionString)
 
         val client = account.createCloudBlobClient()
-        val container = client.getContainerReference("thumbnails")
+        val container = client.getContainerReference(containerName)
 
         val blob = container.getBlockBlobReference(imageId)
         val outputStream = ByteArrayOutputStream()
