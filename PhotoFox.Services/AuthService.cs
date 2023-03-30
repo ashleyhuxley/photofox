d252c5fc5ea942330128947845cb1a5a21365e57
Ashley Huxley <git@huxley.im>
2023-03-31 00:04:11 +0100

    Added AuthService tests

--- PhotoFox.Services/AuthService.cs
+++ PhotoFox.Services/AuthService.cs
@@ -1,4 +1,5 @@
 ﻿using PhotoFox.Storage.Table;
+using System;
 using System.Threading.Tasks;
 
 namespace PhotoFox.Services
@@ -9,7 +10,7 @@ namespace PhotoFox.Services
 
         public AuthService(IAlbumPermissionStorage albumPermissionStorage)
         {
-            this.albumPermissionStorage = albumPermissionStorage;
+            this.albumPermissionStorage = albumPermissionStorage ?? throw new ArgumentNullException(nameof(albumPermissionStorage));
         }
 
         public async Task<bool> HasPermission(string albumId, string username)
