de7d535f0cee099956be5443757e0dc964362d06
Ashley Huxley <git@huxley.im>
2023-03-30 23:21:52 +0100

    Added tests

---
+++ PhotoFox.Core.UnitTests/StreamHashTests.cs
@@ -0,0 +1,31 @@
+﻿using PhotoFox.Core.Hashing;
+
+namespace PhotoFox.Core.UnitTests
+{
+    public class StreamHashTests
+    {
+        [Test]
+        public void ComputeHash_ValidInput_ReturnsCorrectHash()
+        {
+            var hash = new StreamHashMD5();
+
+            byte[] input = new byte[] { 1, 2, 3, 4, 5, 6 };
+            using (var stream = new MemoryStream(input))
+            {
+                Assert.That(hash.ComputeHash(stream), Is.EqualTo("6AC1E56BC78F031059BE7BE854522C4C"));
+            }
+        }
+
+        [Test]
+        public void ComputeHash_EmptyInput_DoesNotThrow()
+        {
+            var hash = new StreamHashMD5();
+
+            byte[] input = Array.Empty<byte>();
+            using (var stream = new MemoryStream(input))
+            {
+                Assert.DoesNotThrow(() => hash.ComputeHash(stream));
+            }
+        }
+    }
+}
--- PhotoFox.Core/Extensions/LongExtensions.cs
+++ PhotoFox.Core/Extensions/LongExtensions.cs
@@ -7,13 +7,21 @@
             string[] sizes = { "B", "KB", "MB", "GB", "TB" };
 
             int order = 0;
-            while (bytes >= 1024 && order < sizes.Length - 1)
+            while (bytes >= 1048576 && order < sizes.Length - 1)
             {
                 order++;
                 bytes = bytes / 1024;
             }
 
-            return string.Format("{0:0.##} {1}", bytes, sizes[order]);
+            double result = bytes;
+
+            if (bytes >= 1024)
+            {
+                order++;
+                result = bytes / 1024f;
+            }
+
+            return string.Format("{0:0.##} {1}", result, sizes[order]);
         }
     }
 }
