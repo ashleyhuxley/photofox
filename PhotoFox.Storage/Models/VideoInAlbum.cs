273c0d1e2694eb51b10c87610fb453a03bff5781
Ashley Huxley <git@huxley.im>
2023-04-01 17:25:18 +0100

    Added VideoInAlbum storage

---
+++ PhotoFox.Storage/Models/VideoInAlbum.cs
@@ -0,0 +1,21 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using System;
+
+namespace PhotoFox.Storage.Models
+{
+    public class VideoInAlbum : ITableEntity
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+        public string Title { get; set; }
+        public DateTime? VideoDate { get; set; }
+        public int FileSize { get; set; }
+        public double? GeolocationLattitude { get; set; }
+        public double? GeolocationLongitude { get; set; }
+
+
+    }
+}
---
+++ PhotoFox.Storage/Table/IVideoInAlbumStorage.cs
@@ -0,0 +1,13 @@
+﻿using Azure;
+using PhotoFox.Storage.Models;
+using System.Threading.Tasks;
+
+namespace PhotoFox.Storage.Table
+{
+    public interface IVideoInAlbumStorage
+    {
+        AsyncPageable<VideoInAlbum> GetVideosInAlbumAsync(string albumId);
+        Task AddVideoInAlbumAsync(VideoInAlbum videoInAlbum);
+        Task ModifyVideoInAlbumAsync(VideoInAlbum videoInAlbum);
+    }
+}
