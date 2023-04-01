1bf669183800271d647647eb3635b88ab83217ee
Ashley Huxley <git@huxley.im>
2023-04-01 17:25:18 +0100

    Added VideoInAlbum storage

---
+++ PhotoFox.Storage/Table/VideoInAlbumStorage.cs
@@ -0,0 +1,40 @@
+﻿using Azure.Data.Tables;
+using Azure;
+using PhotoFox.Storage.Models;
+using System.Threading.Tasks;
+
+namespace PhotoFox.Storage.Table
+{
+    public class VideoInAlbumStorage : IVideoInAlbumStorage
+    {
+        private readonly IStorageConfig config;
+
+        private const string TableName = "VideoInAlbum";
+
+        public VideoInAlbumStorage(IStorageConfig storageConfig)
+        {
+            this.config = storageConfig;
+        }
+
+        public AsyncPageable<VideoInAlbum> GetVideosInAlbumAsync(string albumId)
+        {
+            var client = new TableServiceClient(config.StorageConnectionString);
+            var tableClient = client.GetTableClient(TableName);
+            return tableClient.QueryAsync<VideoInAlbum>(p => p.PartitionKey == albumId);
+        }
+
+        public async Task AddVideoInAlbumAsync(VideoInAlbum videoInAlbum)
+        {
+            var client = new TableServiceClient(config.StorageConnectionString);
+            var tableClient = client.GetTableClient(TableName);
+            await tableClient.AddEntityAsync(videoInAlbum).ConfigureAwait(false);
+        }
+
+        public async Task ModifyVideoInAlbumAsync(VideoInAlbum videoInAlbum)
+        {
+            var client = new TableServiceClient(config.StorageConnectionString);
+            var tableClient = client.GetTableClient(TableName);
+            await tableClient.UpdateEntityAsync(videoInAlbum, ETag.All).ConfigureAwait(false);
+        }
+    }
+}
