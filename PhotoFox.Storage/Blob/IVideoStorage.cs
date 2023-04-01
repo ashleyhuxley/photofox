9ccc2f404c105edfd492cf0723e067b126b8707c
Ashley Huxley <git@huxley.im>
2023-04-01 17:04:40 +0100

    Added a Video Storage class

---
+++ PhotoFox.Storage/Blob/IVideoStorage.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using System;
+
+namespace PhotoFox.Storage.Blob
+{
+    public interface IVideoStorage
+    {
+        Task<BinaryData> GetVideoAsync(string id);
+        Task PutVideoAsync(string id, BinaryData data);
+        Task DeleteVideoAsync(string id);
+    }
+}
--- PhotoFox.Storage/Blob/PhotoFileStorage.cs
+++ PhotoFox.Storage/Blob/PhotoFileStorage.cs
@@ -4,17 +4,15 @@ using System.Threading.Tasks;
 
 namespace PhotoFox.Storage.Blob
 {
-    public class PhotoFileStorage : IPhotoFileStorage
+    public class PhotoFileStorage : StorageBase, IPhotoFileStorage
     {
-        private readonly IStorageConfig config;
-
         private const string ThumbnailsContainer = "thumbnails";
 
         private const string PhotosContainer = "images";
 
         public PhotoFileStorage(IStorageConfig config)
+            :base(config)
         {
-            this.config = config;
         }
 
         public async Task<BinaryData> GetThumbnailAsync(string id)
@@ -37,30 +35,6 @@ namespace PhotoFox.Storage.Blob
             await PutFileAsync(id, data, PhotosContainer).ConfigureAwait(false);
         }
 
-        private async Task<BinaryData> GetFileAsync(string id, string containerName)
-        {
-            var client = new BlobServiceClient(this.config.StorageConnectionString);
-            var container = client.GetBlobContainerClient(containerName);
-
-            var blob = container.GetBlobClient(id);
-
-            if (!blob.Exists())
-            {
-                return null;
-            }
-
-            var res = await blob.DownloadContentAsync().ConfigureAwait(false);
-            return res.Value.Content;
-        }
-
-        private async Task PutFileAsync(string id, BinaryData data, string containerName)
-        {
-            var client = new BlobServiceClient(this.config.StorageConnectionString);
-            var container = client.GetBlobContainerClient(containerName);
-
-            await container.UploadBlobAsync(id, data).ConfigureAwait(false);
-        }
-
         public async Task DeleteThumbnailAsync(string id)
         {
             await this.DeleteFileAsync(id, ThumbnailsContainer).ConfigureAwait(false);
@@ -69,14 +43,6 @@ namespace PhotoFox.Storage.Blob
         public async Task DeletePhotoAsync(string id)
         {
             await this.DeleteFileAsync(id, PhotosContainer).ConfigureAwait(false);
-        }
-
-        private async Task DeleteFileAsync(string id, string containerName)
-        {
-            var client = new BlobServiceClient(this.config.StorageConnectionString);
-            var container = client.GetBlobContainerClient(containerName);
-
-            await container.DeleteBlobIfExistsAsync(id).ConfigureAwait(false);
         }
     }
 }
