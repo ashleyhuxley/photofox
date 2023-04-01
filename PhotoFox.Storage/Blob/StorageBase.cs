6ccdebdfd296a58eb310c2b93a3f60198f44d0a6
Ashley Huxley <git@huxley.im>
2023-04-01 17:04:40 +0100

    Added a Video Storage class

---
+++ PhotoFox.Storage/Blob/StorageBase.cs
@@ -0,0 +1,48 @@
+﻿using Azure.Storage.Blobs;
+using System.Threading.Tasks;
+using System;
+
+namespace PhotoFox.Storage.Blob
+{
+    public class StorageBase
+    {
+        private readonly IStorageConfig storageConfig;
+
+        public StorageBase(IStorageConfig storageConfig) 
+        {
+            this.storageConfig = storageConfig;
+        }
+
+        protected async Task<BinaryData> GetFileAsync(string id, string containerName)
+        {
+            var client = new BlobServiceClient(this.storageConfig.StorageConnectionString);
+            var container = client.GetBlobContainerClient(containerName);
+
+            var blob = container.GetBlobClient(id);
+
+            if (!blob.Exists())
+            {
+                return null;
+            }
+
+            var res = await blob.DownloadContentAsync().ConfigureAwait(false);
+            return res.Value.Content;
+        }
+
+        protected async Task DeleteFileAsync(string id, string containerName)
+        {
+            var client = new BlobServiceClient(this.storageConfig.StorageConnectionString);
+            var container = client.GetBlobContainerClient(containerName);
+
+            await container.DeleteBlobIfExistsAsync(id).ConfigureAwait(false);
+        }
+
+        protected async Task PutFileAsync(string id, BinaryData data, string containerName)
+        {
+            var client = new BlobServiceClient(this.storageConfig.StorageConnectionString);
+            var container = client.GetBlobContainerClient(containerName);
+
+            await container.UploadBlobAsync(id, data).ConfigureAwait(false);
+        }
+    }
+}
---
+++ PhotoFox.Storage/Blob/VideoStorage.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using System;
+
+namespace PhotoFox.Storage.Blob
+{
+    public class VideoStorage : StorageBase, IVideoStorage
+    {
+        private const string VideosContainer = "videos";
+
+        public VideoStorage(IStorageConfig config)
+            :base(config)
+        {
+        }
+
+        public async Task<BinaryData> GetVideoAsync(string id)
+        {
+            return await GetFileAsync(id, VideosContainer).ConfigureAwait(false);
+        }
+
+        public async Task PutVideoAsync(string id, BinaryData data)
+        {
+            await PutFileAsync(id, data, VideosContainer).ConfigureAwait(false);
+        }
+
+        public async Task DeleteVideoAsync(string id)
+        {
+            await this.DeleteFileAsync(id, VideosContainer).ConfigureAwait(false);
+        }
+    }
+}
