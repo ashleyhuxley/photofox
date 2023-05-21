f581894006f0229d44f73ef107f63aa78cefca7d
Ashley Huxley <git@huxley.im>
2023-05-21 18:23:32 +0100

    Added ratings

--- PhotoFox.Wpf.Ui.Mvvm/Commands/DecrementRatingCommand.cs
+++ PhotoFox.Wpf.Ui.Mvvm/Commands/DecrementRatingCommand.cs
@@ -1,4 +1,6 @@
-﻿using PhotoFox.Services;
+﻿using CommunityToolkit.Mvvm.Messaging;
+using PhotoFox.Services;
+using PhotoFox.Wpf.Ui.Mvvm.Messages;
 using PhotoFox.Wpf.Ui.Mvvm.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -10,9 +12,14 @@ namespace PhotoFox.Wpf.Ui.Mvvm.Commands
     {
         private readonly IPhotoService photoService;
 
-        public DecrementRatingCommand(IPhotoService photoService)
+        private readonly IMessenger messenger;
+
+        public DecrementRatingCommand(
+            IPhotoService photoService,
+            IMessenger messenger)
         {
             this.photoService= photoService;
+            this.messenger= messenger;
         }
 
         public event EventHandler? CanExecuteChanged;
@@ -35,6 +42,8 @@ namespace PhotoFox.Wpf.Ui.Mvvm.Commands
                 var newRating = await photoService.DecrementRatingAsync(photoViewModel.Item.DateTaken, photoViewModel.Item.PhotoId);
                 photoViewModel.OverrideStarRating(newRating);
             }
+
+            this.messenger.Send(new RefreshVisiblePhotosMessage());
         }
     }
 }
